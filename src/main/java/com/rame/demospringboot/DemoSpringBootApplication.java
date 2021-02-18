719bf9263638f89029e7cf8f09f4f137c89ad82e
Sita-Maha-Lakshmi-Porumalla <68999571+Sita-Maha-Lakshmi-Porumalla@users.noreply.github.com>
2021-02-18 12:26:22 +0530

    Add files via upload

--- src/main/java/com/rame/demospringboot/DemoSpringBootApplication.java
+++ src/main/java/com/rame/demospringboot/DemoSpringBootApplication.java
@@ -2,7 +2,9 @@ package com.rame.demospringboot;
 
 
 import com.rame.demospringboot.model.Customer;
+import com.rame.demospringboot.model.Student;
 import com.rame.demospringboot.repository.CustomerRepository;
+import com.rame.demospringboot.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +16,9 @@ public class DemoSpringBootApplication {
 
 	@Autowired
 	private CustomerRepository custRepo;
+
+	@Autowired
+	private StudentRepository studentRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBootApplication.class, args);
@@ -29,6 +34,15 @@ public class DemoSpringBootApplication {
 			custRepo.save(new Customer("Bill", "Gates"));
 			custRepo.save(new Customer("Rob", "Johnson"));
 			custRepo.save(new Customer("Jeff", "Bezos"));
+
+
+			//Student records
+			studentRepo.save(new Student("Sita", "Porumalla", "Sita123@gmail.com", 20));
+			studentRepo.save(new Student("Maha", "Lakshmi", "Maha789@gmail.com", 20));
+			studentRepo.save(new Student("Jhansi", "Shoe", "Jhansi456@gmail.com", 21));
+			studentRepo.save(new Student("Sweety", "Shetty", "Swetty852@gmail.com", 22));
+
+
 		};
 	}
 }
