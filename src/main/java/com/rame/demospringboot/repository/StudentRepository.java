3ac17a2a2c611c56fc31023828b43f7b5d416111
Sita-Maha-Lakshmi-Porumalla <68999571+Sita-Maha-Lakshmi-Porumalla@users.noreply.github.com>
2021-02-19 20:44:36 +0530

    .

--- src/main/java/com/rame/demospringboot/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rame.demospringboot.repository;
-import com.rame.demospringboot.model.Student;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends CrudRepository<Student, Long> {
-
-}
